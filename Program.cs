c98ae570ff60896581817357ca1beff51b2a3d79
Mirko <mirkoat21@gmail.com>
2023-06-28 14:41:15 +0200

    final

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ using Microsoft.ML.Data;
 using Spectre.Console;
 using Console = Spectre.Console.AnsiConsole;
 using System.Reactive.Concurrency;
+using System.Reactive;
+using System;
 
 namespace Yelp_restorani
 {
@@ -34,25 +36,60 @@ namespace Yelp_restorani
     public class Business
     {
         public string? Name { get; set; }
+        public Business(string? name)
+        {
+            Name = name;
+        }
     }
     public class BusinessObserver : IObserver<Business>
     {
         private readonly string name;
+        private int positiveCount;
+        private int negativeCount;
+
         public BusinessObserver(string name)
         {
             this.name = name;
+            positiveCount = 0;
+            negativeCount = 0;
         }
+
         public void OnNext(Business business)
         {
-            Console.WriteLine("Presli smo na sledeci biznis");
+            Console.WriteLine("Passed on next business, now completed: " + business.Name+".");
         }
+
         public void OnError(Exception error)
         {
             Console.WriteLine(error.Message);
         }
+
         public void OnCompleted()
         {
-            Console.WriteLine($"{name}: Uspesno zavrsen");
+            Console.WriteLine($"{name}: Complete!");
+            PrintReviewStatistics();
+        }
+
+        public void AddReviewSentiment(bool isPositive)
+        {
+            if (isPositive)
+            {
+                positiveCount++;
+            }
+            else
+            {
+                negativeCount++;
+            }
+        }
+
+        private void PrintReviewStatistics()
+        {
+            int totalCount = positiveCount + negativeCount;
+            double positivePercentage = (double)positiveCount / totalCount * 100;
+            double negativePercentage = (double)negativeCount / totalCount * 100;
+
+            Console.WriteLine($"Positive reviews: {positiveCount} ({positivePercentage:F2}%)");
+            Console.WriteLine($"Negative reviews: {negativeCount} ({negativePercentage:F2}%)");
         }
     }
     public class BusinessObservable : IObservable<Business>
@@ -66,7 +103,7 @@ namespace Yelp_restorani
         {
             return subject.Subscribe(observer);
         }
-        public async void GetBusinesses(string location, IScheduler scheduler)
+        public async void GetBusinesses(string location, IScheduler scheduler, BusinessObserver observer)
         {
             #region ML.NET Training
             // ML model training
@@ -99,7 +136,7 @@ namespace Yelp_restorani
                     model = estimator.Fit(splitDataView.TrainSet);
                     var predictions = model.Transform(splitDataView.TestSet);
 
-                    rule.Title = "🏁 Training Complete, Evaluating Accuracy.";
+                    rule.Title = "Training Complete, Evaluating Accuracy.";
                     console.Refresh();
 
                     // evaluate the accuracy of our model
@@ -107,7 +144,7 @@ namespace Yelp_restorani
 
                     var table = new Table()
                         .MinimalBorder()
-                        .Title("💯 Model Accuracy");
+                        .Title("Model Accuracy");
                     table.AddColumns("Accuracy", "Auc", "F1Score");
                     table.AddRow($"{metrics.Accuracy:P2}", $"{metrics.AreaUnderRocCurve:P2}", $"{metrics.F1Score:P2}");
 
@@ -141,6 +178,9 @@ namespace Yelp_restorani
                     {
                         string businessId = business["id"]!.ToString();
                         string businessName = business["name"]!.ToString();
+
+                        Business business1 = new Business(businessName);
+
                         Console.WriteLine($"{businessName} (ID: {businessId})");
 
                         HttpResponseMessage reviewResponse = await client.GetAsync($"https://api.yelp.com/v3/businesses/{businessId}/reviews");
@@ -158,14 +198,19 @@ namespace Yelp_restorani
 
                             var input = new SentimentData { Text = userReview };
                             var result = engine.Predict(input);
-                            var style = result.Prediction ? (color: "green", emoji: "👍") : (color: "red", emoji: "👎");
+                            var style = result.Prediction ? (color: "gray", emoji: "👍") : (color: "gray", emoji: "👎");
 
                             Console.WriteLine($"- {userName}: {userReview}");
-                            Console.MarkupLine($"{style.emoji} [{style.color}]\"\" ({result.Probability:P00})[/] ");
+                            Console.MarkupLine($"[{style.color}] ({result.Probability:P00})[/] ");
+                            
                             Console.WriteLine();
-                            Console.WriteLine();
+
+                            bool isPositive = result.Prediction;
+                            observer.AddReviewSentiment(isPositive);
                         }
+                        observer.OnNext(business1);       
                     }
+                    observer.OnCompleted();
                 }
                 catch (Exception ex)
                 {
@@ -197,7 +242,7 @@ namespace Yelp_restorani
 
             IScheduler scheduler = NewThreadScheduler.Default;
 
-            businessObservable.GetBusinesses(location,scheduler);
+            businessObservable.GetBusinesses(location,scheduler,observer1);
             System.Console.ReadLine();
 
             subscription1.Dispose();
